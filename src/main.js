5744a5485f6f0d65a7a69829bb565897985243f4
Frost Bound <sxcr3am@gmail.com>
2026-06-10 13:01:37 +0530

    Designing Home Page

--- src/main.js
+++ src/main.js
@@ -1,16 +1,47 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <section class="hero">
-    <h1>Senodh Anavaratne</h1>
+document.querySelector("#app").innerHTML = `
+<nav>
+  <div class="logo">SA</div>
+
+  <div class="nav-links">
+    <a href="#">Home</a>
+    <a href="#">About</a>
+    <a href="#">Projects</a>
+    <a href="#">Contact</a>
+  </div>
+</nav>
+
+<section class="hero">
+
+  <div class="hero-left">
+
+    <div class="badge">
+      ● Available for Opportunities
+    </div>
+
+    <h1>
+      Senodh <br />
+      <span>Anavaratne</span>
+    </h1>
+
     <h2>Software Engineering Undergraduate</h2>
+
     <p>
-      Full Stack Developer focused on Django, JavaScript and modern web applications.
+      Full Stack Developer focused on building modern web applications,
+      Django systems and scalable solutions.
     </p>
 
     <div class="buttons">
-      <a href="#projects">Projects</a>
+      <a href="#">View Projects</a>
       <a href="#">Download CV</a>
     </div>
-  </section>
-`
+
+  </div>
+
+  <div class="hero-right">
+    <div class="planet"></div>
+  </div>
+
+</section>
+`;
