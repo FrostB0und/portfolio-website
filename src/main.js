8c1edbd6ffeb42143e8989b84583178575348a7b
Frost Bound <sxcr3am@gmail.com>
2026-06-10 12:54:51 +0530

    Create hero section

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <section class="hero">
+    <h1>Senodh Anavaratne</h1>
+    <h2>Software Engineering Undergraduate</h2>
+    <p>
+      Full Stack Developer focused on Django, JavaScript and modern web applications.
+    </p>
+
+    <div class="buttons">
+      <a href="#projects">Projects</a>
+      <a href="#">Download CV</a>
+    </div>
+  </section>
+`
